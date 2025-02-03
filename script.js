a3f582993047341e8e19aee40142c6d59a438f6a
tomjia98 <tomjia1998@gmail.com>
2025-02-03 14:55:44 +0800

    added descriptions and modals for all cards

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+function scrollToCard(cardId) {
+  var cardElement = document.getElementById(cardId);
+  if (cardElement) {
+      var offset = 500; // Adjust this based on your navbar height
+      var elementPosition = cardElement.getBoundingClientRect().top + window.scrollY;
+      var offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+
+      // Update URL without reloading
+      window.history.pushState(null, null, '#' + cardId);
+  }
+}
