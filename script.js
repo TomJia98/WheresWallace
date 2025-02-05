b0021f9d766c1a30e1bc55aa4989917d15b2a064
tomjia98 <tomjia1998@gmail.com>
2025-02-05 18:33:04 +0800

    added gallery, but need to handle closing image

--- script.js
+++ script.js
@@ -18,3 +18,36 @@ function scrollToCard(cardId) {
     window.history.pushState(null, null, '#' + cardId);
   }
 }
+
+function openModal(imgElement) {
+  const modalImage = document.getElementById("modalImage");
+  modalImage.src = imgElement.src;
+
+  const imageModal = new bootstrap.Modal(document.getElementById("imageModal"));
+  imageModal.show();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Track when a modal is opened
+  document.querySelectorAll(".modal").forEach(modal => {
+      modal.addEventListener("shown.bs.modal", function () {
+          history.pushState({ modalOpen: true }, "", "#modal");
+      });
+
+      modal.addEventListener("hidden.bs.modal", function () {
+          // Remove state when the modal closes
+          if (history.state && history.state.modalOpen) {
+              history.back();
+          }
+      });
+  });
+
+  // Detect when user presses "Back"
+  window.addEventListener("popstate", function (event) {
+      let openModal = document.querySelector(".modal.show");
+      if (openModal) {
+          let modalInstance = bootstrap.Modal.getInstance(openModal);
+          modalInstance.hide();
+      }
+  });
+});
