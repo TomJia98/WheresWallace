df90721c0b954eba788f90c2e11e506152374185
TomJia98 <tomjia1998@gmail.com>
2025-05-28 12:27:43 +0800

    changed image size, updated descriptions and added social media button

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const myModal = document.getElementById("myModal");
 const myInput = document.getElementById("myInput");
+const galleryDiv = document.getElementById("gallery-container");
+
+const galleryImages = [
+"images/3f74942e-84e6-47ea-8848-3293b708b755.jpg",
+"images/5f6fa9ad-1132-4ad5-a110-822e4bec08e7.jpg",
+"images/48fbdadb-2b61-4a2c-8085-ef38c4166120.jpg",
+"images/57f8e273-efe9-4946-b4e5-c4543184d421.jpg",
+"images/263c5c04-9243-4ef0-852e-d4b41c9f59b1.jpg",
+"images/076710e6-c0f9-4827-815b-668ede619691.jpg",
+"images/b9772c7a-13b0-4587-a870-2a929a5c585c.jpg",
+"images/ba979b32-72bd-4f4b-8c0b-81713d9bac07.jpg",
+"images/c15e8bba-59d2-4fc4-803b-e46abf60b082.jpg",
+"images/cd2afddb-cdb2-427b-b20e-f56e7414ceab.jpg",
+"images/gardening before.jpg",
+"images/gardening after.jpg",
+"images/gardening2.jpg",
+"images/lawn1.jpg",
+"images/lawn2.jpg",
+]
+
+galleryImages.forEach(image => {
+  const imageElement = ` <img src="${image}" class="gallery-item showmodal" data-show-modal="imageModal">`;
+galleryDiv.innerHTML += imageElement;
+})
+
 
 const cardInfo = [
   {
@@ -7,7 +32,7 @@ const cardInfo = [
     target: "lawnmowingModal",
     image: "images/57f8e273-efe9-4946-b4e5-c4543184d421.jpg",
     title: "Lawnmowing",
-    description: "Lawnmowing description",
+    description: "Regular lawn mowing can be a time consuming task, but with our team it will be no hassle at all.",
   },
   {
     id: "acreage",
@@ -19,7 +44,7 @@ const cardInfo = [
   {
     id: "lawncare",
     target: "lawncareModal",
-    image: "https://placehold.co/600x400",
+    image: "images/lawn1.jpg",
     title: "Lawncare & Maintenance",
     description:
       "Transform your lawn with our extensive care and maintenance services.",
@@ -29,7 +54,8 @@ const cardInfo = [
     target: "gardeningModal",
     image: "images/263c5c04-9243-4ef0-852e-d4b41c9f59b1.jpg",
     title: "Gardening",
-    description: "Gardening description",
+    description: `Revitalize your garden or design an entirely new outdoor space for your
+                          home or business`,
   },
   {
     id: "window",
@@ -42,30 +68,30 @@ const cardInfo = [
   {
     id: "pressure",
     target: "pressureModal",
-    image: "https://placehold.co/600x400",
+    image: "images/pressure.jpg",
     title: "Pressure Washing",
     description:
       "Restore and maintain your property with our professional pressure washing services.",
   },
   {
     id: "soft",
-    target: "#softModal",
-    image: "https://placehold.co/600x400",
+    target: "softModal",
+    image: "images/soft.jpg",
     title: "Soft Washing",
     description: "Refresh and protect your property with expert soft washing.",
   },
   {
     id: "cleaning",
-    target: "#cleaningModal",
-    image: "https://placehold.co/600x400",
+    target: "cleaningModal",
+    image: "images/cleaning.jpg",
     title: "Commercial & Residential Cleaning",
     description:
       "Keep your space spotless with our professional cleaning services.",
   },
   {
     id: "blind",
-    target: "#blindModal",
-    image: "https://placehold.co/600x400",
+    target: "blindModal",
+    image: "images/blinds.jpg",
     title: "Ultrasonic Blind Cleaning",
     description: "Transform your blinds with cutting-edge ultrasonic cleaning.",
   },
@@ -76,9 +102,9 @@ const modalInfo = [
     id: "lawncareModal",
     label: "lawncareModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+      "images/lawn1.jpg",
+      "images/b9772c7a-13b0-4587-a870-2a929a5c585c.jpg",
+    
     ],
     title: "Lawncare and Maintainance",
     desc: `  
@@ -93,9 +119,9 @@ const modalInfo = [
     id: "acreageModal",
     label: "acreageModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+      "images/3f74942e-84e6-47ea-8848-3293b708b755.jpg",
+      "images/cd2afddb-cdb2-427b-b20e-f56e7414ceab.jpg",
+      
     ],
     title: "Acreage Mowing",
     desc: `  
@@ -110,9 +136,9 @@ const modalInfo = [
     id: "windowModal",
     label: "windowModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+      "images/5f6fa9ad-1132-4ad5-a110-822e4bec08e7.jpg",
+      "images/076710e6-c0f9-4827-815b-668ede619691.jpg",
+      "images/48fbdadb-2b61-4a2c-8085-ef38c4166120.jpg",
     ],
     title: "Window Cleaning",
     desc: `  
@@ -129,9 +155,7 @@ const modalInfo = [
     id: "pressureModal",
     label: "pressureModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+        "images/pressure.jpg",
     ],
     title: "Pressure Washing",
     desc: `  
@@ -148,9 +172,7 @@ const modalInfo = [
     id: "softModal",
     label: "softModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+    "images/soft.jpg"
     ],
     title: "Soft Washing",
     desc: `  
@@ -167,9 +189,7 @@ const modalInfo = [
     id: "cleaningModal",
     label: "cleaningModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+  "images/cleaning.jpg"
     ],
     title: "Commercial and Residential Cleaning",
     desc: `  
@@ -186,9 +206,7 @@ const modalInfo = [
     id: "blindModal",
     label: "blindModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+      "images/blinds.jpg"
     ],
     title: "Ultrasonic Blind Cleaning",
     desc: `  
@@ -205,26 +223,33 @@ const modalInfo = [
     id: "gardeningModal",
     label: "gardeningModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+      "images/gardening5.jpg",
+      "images/263c5c04-9243-4ef0-852e-d4b41c9f59b1.jpg",
+      "images/gardening2.jpg",
+      "images/ba979b32-72bd-4f4b-8c0b-81713d9bac07.jpg"
     ],
     title: "Gardening",
     desc: `  
-                          gardening desc`,
+                          Whether you're looking to revitalize your garden or design an entirely new outdoor space for your
+                          home or business, you can trust us to bring your vision to life. With years of expertise in every aspect
+                           of gardening and landscaping, we work closely with you to ensure the rresluts not only meet but exceed
+                           your expectations. Our commitmen to quality and attention to detail guarantees that you'll recieve the 
+                           outdoor space you've always dreamed of.`,
   },
 
   {
     id: "lawnmowingModal",
     label: "lawnmowingModalLabel",
     images: [
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
-      "https://placehold.co/600x400",
+      "images/57f8e273-efe9-4946-b4e5-c4543184d421.jpg",
+      "images/lawn2.jpg",
+     
     ],
     title: "Lawnmowing",
     desc: `  
-                          lawnmowing desc`,
+                          Regular lawn mowing can be a time consuming task, but with our team it will be no hassle at all.
+                          We offer customisable scheduling options to fit your lifestyle, and your lawn's neeeds so that it
+                          stays maintained all year round without you having to worry at all.`,
   },
 ];
 
@@ -242,7 +267,7 @@ cardInfo.forEach(card => {
   const cardElement = `
   <div class="col-lg-4 col-md-6 col-sm-12" id="${card.id}" data-bs-toggle="modal" data-bs-target="#${card.target}">
                 <div class="card hover">
-                    <img src="${card.image}" class="card-img-top" alt="${card.title} image">
+                    <img src="${card.image}" class="cardimgtop" alt="${card.title} image">
                     <div class="card-body">
                         <h5 class="card-title">${card.title}</h5>
                         <p class="card-text">${card.description}</p>
@@ -271,16 +296,13 @@ modalInfo.forEach(modal => {
                                 </div>
                     <!-- Carousel -->
                     <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-
-                        ${modal.images.map((img, index) => `
-                          <div class="carousel-item ${index === 0 ? 'active' : ''}">
-                              <img src="${img}" class="d-block w-100" alt="...">
-                          </div>
-                      `).join("")}
-                      
-                           
-                        </div>
+                    <div class="carousel-inner">
+  ${modal.images.map((img, index) => `
+    <div class="carousel-item d-flex justify-content-center ${index === 0 ? 'active' : ''}">
+      <img src="${img}" class="imageStyle" alt="...">
+    </div>
+  `).join("")}
+</div>
                     </div>
 
                     <!-- Close Button -->
